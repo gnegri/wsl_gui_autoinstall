99684a83b7b82536c0c120248f289fc20b884208
Grant Negri <7268992+gnegri@users.noreply.github.com>
2020-12-03 00:21:50 -0500

    pulseaudio only

--- wsl_pulseaudio_autoinstall.bat
+++ wsl_pulseaudio_autoinstall.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 
-set WSLENV=%WSLENV%:WINTMP
+set WSLENV=%WSLENV%:WINTMP:WINAPP
 set WINTMP=%TMP%
 set LINUXTMP='$(wslpath -u \"$WINTMP\")'
+set WINAPP=%AppData%
+set LINUXAPP='$(wslpath -u \"$WINAPP\")'
 
 ECHO --- Running Linux installation.  You will be prompted for your Ubuntu user's password:
 REM One big long command to be absolutely sure we're not prompted for a password repeatedly
@@ -10,27 +12,12 @@ REM One big long command to be absolutely sure we're not prompted for a password
 echo yes ^| add-apt-repository ppa:aseering/wsl-pulseaudio > "%TMP%\script.sh"
 echo apt-get update >> "%TMP%\script.sh"
 echo apt-get -y install pulseaudio unzip >> "%TMP%\script.sh"
-echo sed -i 's/; default-server =/default-server = 127.0.0.1/' /etc/pulse/client.conf >> "%TMP%\script.sh"
+echo sed -i 's/; default-server =.*/default-server = 127.0.0.1/' /etc/pulse/client.conf >> "%TMP%\script.sh"
 echo sed -i "s$<listen>.*</listen>$<listen>tcp:host=localhost,port=0</listen>$" /etc/dbus-1/session.conf >> "%TMP%\script.sh"
 C:\Windows\System32\bash.exe -c "chmod +x '%LINUXTMP%/script.sh' ; tr -d $'\r' < '%LINUXTMP%/script.sh' | tee '%LINUXTMP%/script_clean.sh'; sudo '%LINUXTMP%/script_clean.sh'"
 
-ECHO --- Downloading required third-party packages
-ECHO --- VcXsrv...
-C:\Windows\System32\bash.exe -xc "wget -cO '%LINUXTMP%/vcxsrv.exe' 'http://downloads.sourceforge.net/project/vcxsrv/vcxsrv/1.18.3.0/vcxsrv-64.1.18.3.0.installer.exe'"
-
-ECHO --- PulseAudio...
+ECHO --- Downloading PulseAudio
 C:\Windows\System32\bash.exe -xc "wget -cO '%LINUXTMP%/pulseaudio.zip' 'http://bosmans.ch/pulseaudio/pulseaudio-1.1.zip'"
-
-ECHO --- Installing packages
-
-ECHO --- Adding link for X Server to Startup Items
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\VcXsrv.lnk');$s.TargetPath='%ProgramFiles%\VcXsrv\vcxsrv.exe';$s.Arguments=':0 -ac -terminate -lesspointer -multiwindow -clipboard -wgl';$s.Save()"
-
-ECHO --- Launching X Server.  DO NOT grant access to any network interfaces if prompted; they are unnecessary.
-START "%userprofile%\Start Menu\Programs\Startup\VcXsrv.lnk"
-
-ECHO --- Adding X environment variable to your .bashrc
-C:\Windows\System32\bash.exe -xc "echo 'export DISPLAY=localhost:0' >> ~/.bashrc"
 
 ECHO --- Extracting PulseAudio
 md "%TMP%\pulseaudio"
@@ -39,14 +26,19 @@ C:\Windows\System32\bash.exe -xc "unzip -o '%LINUXTMP%/pulseaudio.zip' -d '%LINU
 ECHO --- Installing PulseAudio
 xcopy /e "%TMP%\pulseaudio" "%AppData%\PulseAudio"
 
+REM Recomended/required settings
+echo load-module module-native-protocol-tcp auth-ip-acl=172.16.0.0/12 auth-anonymous=1 >> "%AppData%\PulseAudio\etc\pulse\default.pa"
+echo load-module module-esound-protocol-tcp auth-ip-acl=172.16.0.0/12 auth-anonymous=1 >> "%AppData%\PulseAudio\etc\pulse\default.pa"
+C:\Windows\System32\bash.exe -xc "sed -i 's/\(load\-module\ module\-waveout\ .*\)/\1 record=0/' '%LINUXAPP%/PulseAudio/etc/pulse/default.pa'"
+C:\Windows\System32\bash.exe -xc "sed -i 's/; exit-idle-time =.*/exit-idle-time = -1/' '%LINUXAPP%/PulseAudio/etc/pulse/daemon.conf'"
+
 ECHO --- Setting PulseAudio to run at startup
 echo set ws=wscript.createobject("wscript.shell") > "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_pulseaudio.vbe"
 echo ws.run "%AppData%\PulseAudio\bin\pulseaudio.exe --exit-idle-time=-1",0 >> "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_pulseaudio.vbe"
+C:\Windows\System32\bash.exe -xc "echo 'export PULSE_SERVER=tcp:$(grep -oP 'nameserver \K.*' /etc/resolv.conf);' >> ~/.bashrc"
 
-REM Recomended/required settings
-echo load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1 >> "%AppData%\PulseAudio\etc\pulse\default.pa"
 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\start_pulseaudio.vbe"
-ECHO When prompted, DO NOT allow 'pulseaudio' access to any of your networks.  It doesn't need access.
+ECHO When prompted, allow 'pulseaudio' access to your networks. It needs access.
 
 ECHO All Done
 PAUSE
